5b09ba4f40264e2f7aa4bd215027e8857ce56a74
Aniket <73299058+Aniket-x-code@users.noreply.github.com>
2022-08-10 23:01:22 +0530

    Add files via upload

--- server.js
+++ server.js
@@ -1,1 +1,16 @@
-console.log("aniket");
+const http = require('http');
+
+
+const server = http.createServer((req, res)=>
+{
+    console.log(req.url,req.method);
+    res.setHeader('content-type','text/plain');
+    res.write("helllo this is anikets server");
+    res.end();
+
+
+})
+
+server.listen(3000,'localhost',()=>{
+    console.log("server started");
+});
