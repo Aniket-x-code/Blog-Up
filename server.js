84b714e48dac8c4c78cf9fd0c5e264eb63c93a75
Aniket <73299058+Aniket-x-code@users.noreply.github.com>
2022-08-11 22:10:18 +0530

    Add files via upload

--- server.js
+++ server.js
@@ -1,16 +1,39 @@
 const http = require('http');
-
+const fs = require('fs');
+const _= require('lodash');
 
 const server = http.createServer((req, res)=>
 {
-    console.log(req.url,req.method);
-    res.setHeader('content-type','text/plain');
-    res.write("helllo this is anikets server");
-    res.end();
+            // lodash
+            
+            const num = _.random(0, 69);
+            console.log(num);
 
+            const greet= _.once( ()=>{
+                console.log('hello');
+            });
+
+
+            greet();
+            greet();
+
+
+    res.setHeader('content-type','text/html');
+    fs.readFile('./index.html' , (err,data)=>{
+            if(err)
+            {
+            console.log(err);
+            res.end();
+            }
+            else
+            { 
+            res.write(data);
+            res.end();
+            }
+    });
 
 })
 
 server.listen(3000,'localhost',()=>{
-    console.log("server started");
+    console.log("server started at port 3000");
 });
