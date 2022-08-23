bb2815a724550a3714b0bdc92ea1735efe22f0b6
Aniket <73299058+Aniket-x-code@users.noreply.github.com>
2022-08-24 00:57:39 +0530

    Add files via upload

--- app.js
+++ app.js
@@ -6,15 +6,20 @@ const app= express();
 // listening to request 
 app.listen(3000);
 
+// registering view engine
+app.set('view engine','ejs')
+
+
+
 app.get('/',(req,res)=>{
-res.sendFile('./views/index.html',{root: __dirname});
+res.sendFile('./index.html',{root: __dirname});
 });
 
 
 app.get('/about',(req,res)=>{
-    res.sendFile('./views/about.html',{root: __dirname});
+    res.sendFile('./about.html',{root: __dirname});
 });
 
 app.use((req,res)=>{
-    res.status(404).sendFile('./views/404.html',{root:__dirname});
+    res.status(404).res.sendFile('404.html',{root:__dirname});
 })
